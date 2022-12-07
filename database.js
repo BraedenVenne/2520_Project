a759e4c0350229331703e78d5f78bd9d69770f29
Braeden <bvenne@my.bcit.ca>
2022-12-06 22:39:29 -0800

    updated the database with example reminders

--- database.js
+++ database.js
@@ -1,10 +1,75 @@
-let Database = {
-    cindy: {
-        reminders: [{id: 1, title: "abc", description: "abcabc", completed: false}]
+const database = [
+    {
+      id: 1,
+      name: "Jimmy Smith",
+      email: "jimmy123@gmail.com",
+      password: "jimmy123!",
+      reminders: [
+        {
+            id: 1,
+            title: "esakgweg",
+            description: "Gafssafdfsd",
+            completed: false,
+        },
+        {
+            id: 2,
+            title: "asasdfsfd",
+            description: "safdadsfsfd",
+            completed: false,
+        },
+      ]
     },
-    alex: {
-        reminders: []
-    } 
-}
-
-module.exports = Database;
+    {
+      id: 2,
+      name: "Johnny Doe",
+      email: "johnny123@gmail.com",
+      password: "johnny123!",
+      reminders: [
+        {
+            id: 1,
+            title: "asasdfsfd",
+            description: "safdadsfsfd",
+            completed: false,
+        },
+        {
+            id: 2,
+            title: "asasdfsfd",
+            description: "safdadsfsfd",
+            completed: false,
+        },
+      ]
+    },
+    {
+      id: 3,
+      name: "Jonathan Chen",
+      email: "jonathan123@gmail.com",
+      password: "jonathan123!",
+      reminders: [
+        {
+            id: 1,
+            title: "asasdfsfd",
+            description: "safdadsfsfd",
+            completed: false,
+        },
+      ],
+    },
+  ];
+  
+  const userModel = {
+    findOne: (email) => {
+      const user = database.find((user) => user.email === email);
+      if (user) {
+        return user;
+      }
+      throw new Error(`Couldn't find user with email: ${email}`);
+    },
+    findById: (id) => {
+      const user = database.find((user) => user.id === id);
+      if (user) {
+        return user;
+      }
+      throw new Error(`Couldn't find user with id: ${id}`);
+    },
+  };
+  
+  module.exports = { database, userModel };
